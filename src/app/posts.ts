4e727c0f7d34f20262a5c82661663ae462d1a82f
Rakesh4vi <rakesh4vi@gmail.com>
2022-10-29 22:49:43 +0530

    Posts Model class updated.

--- src/app/posts.ts
+++ src/app/posts.ts
@@ -1,8 +1,8 @@
 
 
 export class Posts{
- public id: number=0;
- public title: string='';
- public body: string='';
- public userId: number=0
+ public id!: number;
+ public title!: string;
+ public body!: string;
+ public userId!: number;
 }
